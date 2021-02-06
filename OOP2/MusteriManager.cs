67c32adb5d894294395e4f1cd01a2bf63968704f
kilicsule <sule.2607@gmail.com>
2021-02-06 23:15:02 +0300

    23 Ocak Ders Sonu

---
+++ OOP2/MusteriManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    public class MusteriManager
+    {
+        //Is siniflari // business classes
+        public void Add(Musteri musteri)
+        {
+            //if (musteri.)
+            //{
+
+            //}
+        }
+    }
+}
---
+++ OOP2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sule Kilic
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Sule";
+            musteri1.Soyadi = "Kilic";
+            musteri1.TcNo = "124585852";
+
+            //Kodlama.io
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "5445626";
+            musteri2.SirketAdi = "Kodlama.io";
+            musteri2.VergiNo = "45125";
+
+            //Gercek Musteri - Tuzel Musteri
+            //SOLID
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Add(musteri1);
+            musteriManager.Add(musteri2);
+
+
+        }
+    }
+}
